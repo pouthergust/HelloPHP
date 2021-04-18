8a8bb8a2c88c6f9e55da3f4c6d5bf3b0c7c37480
Gabriel Henrique <gabriel.ghvn@gmail.com>
2021-04-17 23:16:55 -0300

    aula07

--- index.php
+++ index.php
@@ -10,21 +10,7 @@
 <body>
     <div>
         <?php 
-            // VARIAVEIS REFERENCIADAS
-            $a = 3;
-            $b = &$a;
-            $b += 5;
 
-            // VARIAVEIS VARIANTES
-            $site = "CursoemVideo";
-            $$site = "CursoPHP"; // nome que essa variavel recebe é CursoemVideo
-
-            echo "variavel site é $site";
-            echo "<br> variavel CursoemVideo é $CursoemVideo";
-
-            // echo "a Vale $a";
-            // echo "<br> b vale $b";
-            
         ?>
     </div>
 </body>
